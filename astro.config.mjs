5c546100bc1ea62d79a354d1c851319ec7ab3aec
yuting <158145233+wyuting0@users.noreply.github.com>
2025-04-20 13:57:08 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://wyuting0.github.io',
+  base: "/",
   output: "static",
   integrations: [tailwind(), icon()],
   output: "server",
